25e541c38ec141de86d89b296887ffb383f40b09
twant <taylor.want@gmail.com>
2016-10-13 20:18:05 +0000

    commented uber.rb

---
+++ uber.rb
@@ -0,0 +1,15 @@
+#Welcome to the Uber lab! Today you will be writing a method to help Uber calculate how much to charge its customers. Uber's model charges people by how far they travel (mileage) and how long they are in the car. Some parts of the method have been coded or commented in for you. Your job is to write the rest. You can test the accuracy of your model at: https://www.uber.com/cities/denver 
+
+#Important info: Uber in Denver charges a $7.00 base fare, $0.35 per minute, and $3.20 per mile
+def uber(mileage, minutes)
+  base_fare=7
+  
+  
+  
+end
+
+#call your Uber method here
+
+#bonus: Uber has a $15 minimum fare. Use an if/else statement to ensure that if the total comes out to be less than $15, the method automatically returns $15.
+
+#double bonus: add an additional parameter type. If the type inputted is "UberBlack", charge the normal rates above. If the type inputted is "UberX", charge $1.00 per mile, $0.13 per minute, and $0.75 base fare. 
